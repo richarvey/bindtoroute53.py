1cf184b23e9e3cdd6495b6884fccfca2ce5fe467
Harvey <rjh@amazon.com>
2017-12-11 09:46:30 +0000

    tody up mail use gh to connect

--- bindtoroute53.py
+++ bindtoroute53.py
@@ -16,7 +16,6 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-# e-mail:richard@squarecows.com
 
 import dns.zone, sys, getopt, os, time, datetime
 
